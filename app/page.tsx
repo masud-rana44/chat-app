c64a2377f1b64702eed9a97af015a2e7f2178c59
masud-rana44 <mdmasudrana4488@gmail.com>
2025-12-17 22:24:19 +0600

    Setting up clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <h1>
       <Button>Click Me</Button>
+      <UserButton />
     </h1>
   );
 }
